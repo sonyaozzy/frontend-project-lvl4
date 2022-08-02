19e355cb0b1f3dbf63cdedb66d70d6595b553996
Anna Papiashvili <an.papiashvili@gmail.com>
2022-08-02 16:05:36 +0400

    Second Step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+            <Link to="/will-not-match">Will Not Match</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Login() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+      password: Yup.string()
+        .min(6, 'Must be 6 characters or more')
+        .required('Required'),
+    }),
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <div className="card-body row p-5">
+    <form className="col-12 col-md-6 mt-3 mt-mb-0" onSubmit={formik.handleSubmit}>
+      <h1 class="text-center mb-4">Войти</h1>
+      <div class="form-floating mb-3">
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Your Name"
+        className="form-control"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.name}
+      />
+      <label htmlFor="name">Name</label>
+      {formik.touched.name && formik.errors.name ? (
+        <div>{formik.errors.name}</div>
+      ) : null}
+      </div>
+
+      <div class="form-floating mb-4">
+      <input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="Password"
+        className="form-control"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      <label className="form-label"  htmlFor="password">Password</label>
+      {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
+      </div>
+      <button className="w-100 mb-3 btn btn-outline-primary" type="submit">Sign in</button>
+    </form>
     </div>
   );
 }
 
-export default App;
+function NoMatch() {
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100">
+            <div className="text-center">
+                <h1 className="display-1 fw-bold">404</h1>
+                <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+                <p className="lead">
+                    The page you’re looking for doesn’t exist.
+                  </p>
+                <a href="/" className="btn btn-primary">Go Home</a>
+            </div>
+        </div>
+  )
+}
