017029287c89f128c9084ba95d3804d45612c5c3
Anna Papiashvili <an.papiashvili@gmail.com>
2023-04-19 23:23:54 +0400

    add last changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,17 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import routes from '../routes.js';
+
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'));
+
+  if (userId?.token) {
+    return { Authorization: `Bearer ${userId.token}` };
+  }
+
+  return {};
+};
 
 const Home = () => {
   const navigate = useNavigate();
@@ -11,6 +23,13 @@ const Home = () => {
       navigate('/login');
     }
   }, [navigate]);
+
+  useEffect(() => {
+    axios.get(routes.dataPath(), { headers: getAuthHeader() }).then((response) => {
+      console.log(response.data); // => { channels: [{id: 1, name: 'general', removable: false},
+      // {id: 2, name: 'random', removable: false}], currentChannelId: 1, messages: [] }
+    });
+  }, []);
 
   return <h2>Home</h2>;
 };
